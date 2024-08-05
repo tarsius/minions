413b95a0d1c7c10d0f8d440d1982062b73d5ea4a
Jonas Bernoulli <jonas@bernoul.li>
2024-08-05 16:22:44 +0200

    Release version 1.0.2

--- minions.el
+++ minions.el
@@ -6,6 +6,7 @@
 ;; Homepage: https://github.com/tarsius/minions
 ;; Keywords: convenience
 
+;; Package-Version: 1.0.2
 ;; Package-Requires: ((emacs "26.1") (compat "30.0.0.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
